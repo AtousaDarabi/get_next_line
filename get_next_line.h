e945df50b9a097d6d878e749f0ac051f9212ad98
Atousa Darabi <adarabi@3-A-25.42heilbronn.de>
2026-05-18 22:02:46 +0200

    Added utils functions.

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,20 @@
 /*   By: adarabi <adarabi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/18 18:51:44 by adarabi           #+#    #+#             */
-/*   Updated: 2026/05/18 18:51:45 by adarabi          ###   ########.fr       */
+/*   Updated: 2026/05/18 22:01:50 by adarabi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+void    ft_strlcpy(char *dst, char *src, size_t size);
+size_t  ft_strlen(char *str);
+int     ft_chrfind(char *str, int c);
+char    *get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+
+#endif
